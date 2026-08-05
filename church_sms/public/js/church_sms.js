82955aad12fd955fc79b803300b3c0b413fd8c2a
GoldenJustin <justinemsengi@gmail.com>
2026-08-05 17:21:51 +0000

    fix: Pass manual_numbers field to API

--- church_sms/public/js/church_sms.js
+++ church_sms/public/js/church_sms.js
@@ -3,7 +3,6 @@ frappe.ui.form.on('Church SMS', {
         // Show/hide fields based on send_to
         frm.toggle_display('branch', frm.doc.send_to === 'Specific Branch');
         frm.toggle_display('manual_numbers', frm.doc.send_to === 'Manual Numbers');
-        frm.toggle_display('members', frm.doc.send_to === 'Specific Members');
         
         // Make fields mandatory
         frm.toggle_reqd('branch', frm.doc.send_to === 'Specific Branch');
@@ -15,39 +14,48 @@ frappe.ui.form.on('Church SMS', {
         frm.trigger('send_to');
         
         // Add Send SMS button
-        if (!frm.is_new() && frm.doc.status === 'Draft') {
+        if (!frm.is_new() && frm.doc.docstatus === 0) {
             frm.add_custom_button(__('Send SMS'), function() {
+                // Prepare arguments
+                var args = {
+                    send_to: frm.doc.send_to,
+                    message: frm.doc.message,
+                    sender_id: frm.doc.sender_id || 'KKKT MABIBO',
+                    branch: frm.doc.branch || '',
+                    members: frm.doc.members || []
+                };
+                
+                // Add manual_numbers if applicable
+                if (frm.doc.send_to === 'Manual Numbers') {
+                    args.manual_numbers = frm.doc.manual_numbers || '';
+                }
+                
                 frappe.call({
                     method: 'church_sms.api.send_church_sms',
-                    args: {
-                        send_to: frm.doc.send_to,
-                        message: frm.doc.message,
-                        sender_id: frm.doc.sender_id,
-                        branch: frm.doc.branch || '',
-                        members: frm.doc.members || [],
-                        manual_numbers: frm.doc.manual_numbers || ''
-                    },
+                    args: args,
                     freeze: true,
                     freeze_message: __('Sending SMS...'),
                     callback: function(r) {
-                        if (r.message && r.message.success) {
-                            frappe.msgprint({
-                                title: __('Success'),
-                                indicator: 'green',
-                                message: r.message.message
-                            });
-                            frm.set_value('status', 'Sent');
-                            frm.set_value('response', r.message.message);
-                            frm.save();
-                        } else {
-                            frappe.msgprint({
-                                title: __('Failed'),
-                                indicator: 'red',
-                                message: r.message ? r.message.message : __('Unknown error')
-                            });
-                            frm.set_value('status', 'Failed');
-                            frm.set_value('response', r.message ? r.message.message : 'Failed');
-                            frm.save();
+                        if (r.message) {
+                            if (r.message.success) {
+                                frappe.msgprint({
+                                    title: __('Success'),
+                                    indicator: 'green',
+                                    message: r.message.message
+                                });
+                                frm.set_value('status', 'Sent');
+                                frm.set_value('response', r.message.message);
+                                frm.save();
+                            } else {
+                                frappe.msgprint({
+                                    title: __('Failed'),
+                                    indicator: 'red',
+                                    message: r.message.message
+                                });
+                                frm.set_value('status', 'Failed');
+                                frm.set_value('response', r.message.message);
+                                frm.save();
+                            }
                         }
                     }
                 });
